2c2df86ce66a68db7b249a3e4dd111975b76d0a0
Tiaan van Deventer <tiaanvandeventer@gmail.com>
2016-04-14 14:51:45 +0200

    Returns empty list no results

--- domain_name_gem/spec/provider_spec.rb
+++ domain_name_gem/spec/provider_spec.rb
@@ -23,13 +23,20 @@ describe DomainNameGem::Provider do
     it "should return a list of servers for a given domain name" do
       dnp = DomainNameGem::Provider.new
       dnp.adapter=(FakeAdapter)
-      expect(dnp.servers_domain_hosted_on?('example.com')).to eql ['www10.tst1.host-h.net']
+      expect(dnp.servers_domain_hosted_on?('setup_on_one_server.co.za')).to eql ['www10.tst1.host-h.net']
+    end
+
+    it "Given the domain is not setup on any servers return a empty list" do
+      dnp = DomainNameGem::Provider.new
+      dnp.adapter=(FakeAdapter)
+      expect(dnp.servers_domain_hosted_on?('not_setup_on_any_servers.co.za')).to be_empty
     end
   end
 end
 
 module FakeAdapter
   def self.servers_domain_hosted_on?(domain_name)
-    ['www10.tst1.host-h.net']
+    return ['www10.tst1.host-h.net'] if domain_name == 'setup_on_one_server.co.za'
+    return [] if domain_name == 'not_setup_on_any_servers.co.za'
   end
 end
