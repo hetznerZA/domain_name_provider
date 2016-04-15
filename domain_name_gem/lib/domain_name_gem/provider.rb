1d2636a907330632d07e8e6c7e17a8e2d946cc03
Tiaan van Deventer <tiaanvandeventer@gmail.com>
2016-04-15 15:04:48 +0200

    Support config validation

--- domain_name_gem/lib/domain_name_gem/provider.rb
+++ domain_name_gem/lib/domain_name_gem/provider.rb
@@ -1,13 +1,28 @@
 module DomainNameGem
   class Provider
-    attr_reader :configuration, :adapter
+    attr_reader :adapter, :status
 
     def initialize(configuration)
-      @adapter = configuration[:adapter].new(configuration)
+      @status = 'fail'
+
+      if valid_configuration?(configuration)
+        @adapter = configuration[:adapter][:class_name].new(configuration[:adapter][:configuration])
+
+        if @adapter.valid_configuration?
+          @status = 'success'
+        end
+      end
     end
 
-    def servers_domain_hosted_on?(domain_name)
-      @adapter.servers_domain_hosted_on?(domain_name)
+    def servers_domain_hosted_on(domain_name)
+      @adapter.servers_domain_hosted_on(domain_name)
+    end
+
+    private
+    def valid_configuration?(configuration)
+      return false if configuration[:adapter].nil?
+      return false if configuration[:adapter][:class_name].nil?
+      true
     end
   end
 end
--- domain_name_gem/spec/provider_spec.rb
+++ domain_name_gem/spec/provider_spec.rb
@@ -1,40 +1,74 @@
 require 'spec_helper'
 
 describe DomainNameGem::Provider do
+  context "Initialization" do
+    context "configuration" do
+      context "Given valid configuration" do
+        it "return success status" do
+          valid_configuration = { :adapter => { :class_name => FakeAdapter, :configuration => { :user => 'test_user' } } }
 
-
-  context "When initialized" do
-    context "Configure Adapter" do
-      context "Changing the adapter" do
-        it "override default adapter" do
-          configuration = { :adapter => FakeAdapter }
-          dnp = DomainNameGem::Provider.new(configuration)
-
-          expect(dnp.adapter.class).to eql FakeAdapter
+          dnp = DomainNameGem::Provider.new(valid_configuration)
+          expect(dnp.status).to eql 'success'
         end
       end
 
-      context "Given no adapter is provided" do
-        it "return a default adapter" do
+      context "Given invalid configuration" do
+        it "return an fail status" do
+          invalid_configuration = { :adapter => nil }
+
+          dnp = DomainNameGem::Provider.new(invalid_configuration)
+          expect(dnp.status).to eql 'fail'
+        end
+      end
+    end
+
+    context "Adapter initialization" do
+      context "Given an valid adapter class and configuration" do
+        it "return the correct adapter class name" do
+          configuration = { :adapter => { :class_name => FakeAdapter, :configuration => { :user => 'test' } } }
+          dnp = DomainNameGem::Provider.new(configuration)
+          expect(dnp.adapter.class).to eql FakeAdapter
+        end
+
+        it "return an success status" do
+          configuration = { :adapter => { :class_name => FakeAdapter, :configuration => { :user => 'test' } } }
+          dnp = DomainNameGem::Provider.new(configuration)
+          expect(dnp.status).to eql 'success'
+        end
+      end
+
+      context "Given the adapter class doesn't exist" do
+        it "return something" do
           pending
+          configuration = { :adapter => { :class_name => Adapter, :configuration => { } } }
+          dnp = DomainNameGem::Provider.new(configuration)
+          expect(dnp.status).to eql 'fail'
+        end
+      end
+
+      context "Given invalid adapter configuration" do
+        it "return an fail status" do
+          configuration = { :adapter => { :class_name => FakeAdapter, :configuration => { } } }
+          dnp = DomainNameGem::Provider.new(configuration)
+          expect(dnp.status).to eql 'fail'
         end
       end
     end
   end
 
-  context "#servers_domain_hosted_on?" do
+  context "#servers_domain_hosted_on" do
     it "return a list of servers" do
-      configuration = { :adapter => FakeAdapter }
+      configuration = { :adapter => { :class_name => FakeAdapter, :configuration => { :user => 'test' } } }
       dnp = DomainNameGem::Provider.new(configuration)
 
-      expect(dnp.servers_domain_hosted_on?('setup_on_one_server.co.za')).to eql ['www10.tst1.host-h.net']
+      expect(dnp.servers_domain_hosted_on('setup_on_one_server.co.za')).to eql ['server100.example.com']
     end
 
     it "return an empty list if no results are found" do
-      configuration = { :adapter => FakeAdapter }
+      configuration = { :adapter => { :class_name => FakeAdapter, :configuration => { :user => 'test' } } }
       dnp = DomainNameGem::Provider.new(configuration)
 
-      expect(dnp.servers_domain_hosted_on?('not_setup_on_any_servers.co.za')).to be_empty
+      expect(dnp.servers_domain_hosted_on('not_setup_on_any_servers.co.za')).to be_empty
     end
   end
 end
@@ -44,8 +78,13 @@ class FakeAdapter
     @configuration = configuration
   end
 
-  def servers_domain_hosted_on?(domain_name)
-    return ['www10.tst1.host-h.net'] if domain_name == 'setup_on_one_server.co.za'
+  def valid_configuration?
+    return false if @configuration[:user].nil?
+    true
+  end
+
+  def servers_domain_hosted_on(domain_name)
+    return ['server100.example.com'] if domain_name == 'setup_on_one_server.co.za'
     return [] if domain_name == 'not_setup_on_any_servers.co.za'
   end
 end
