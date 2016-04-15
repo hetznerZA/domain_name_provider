c32a8d4c98d61b733d00e14eaa2d557c831a1b79
Tiaan van Deventer <tiaanvandeventer@gmail.com>
2016-04-15 09:05:27 +0200

    Refactor provider and make simpler
    more cleanup remove configuration remember

--- domain_name_gem/lib/domain_name_gem/provider.rb
+++ domain_name_gem/lib/domain_name_gem/provider.rb
@@ -1,16 +1,13 @@
 module DomainNameGem
   class Provider
-    def adapter
-      return @adapter if @adapter
-      @adapter = 'default_adapter'
-    end
+    attr_reader :configuration, :adapter
 
-    def adapter=(adapter_class)
-      @adapter = adapter_class
+    def initialize(configuration)
+      @adapter = configuration[:adapter].new(configuration)
     end
 
     def servers_domain_hosted_on?(domain_name)
-      self.adapter.servers_domain_hosted_on?(domain_name)
+      @adapter.servers_domain_hosted_on?(domain_name)
     end
   end
 end
--- domain_name_gem/spec/provider_spec.rb
+++ domain_name_gem/spec/provider_spec.rb
@@ -1,41 +1,50 @@
 require 'spec_helper'
 
 describe DomainNameGem::Provider do
-  context "#adapter" do
-    context "Given no adapter is provided" do
-      it "should return a default adapter" do
-        dnp = DomainNameGem::Provider.new
-        expect(dnp.adapter).to eql 'default_adapter'
+
+
+  context "When initialized" do
+    context "Configure Adapter" do
+      context "Changing the adapter" do
+        it "override default adapter" do
+          configuration = { :adapter => FakeAdapter }
+          dnp = DomainNameGem::Provider.new(configuration)
+
+          expect(dnp.adapter.class).to eql FakeAdapter
+        end
       end
-    end
 
-    context "Changing the adapter" do
-      it "should override default adapter" do
-        dnp = DomainNameGem::Provider.new
-        dnp.adapter=('my_new_shiny_adapter')
-
-        expect(dnp.adapter).to eql 'my_new_shiny_adapter'
+      context "Given no adapter is provided" do
+        it "return a default adapter" do
+          pending
+        end
       end
     end
   end
 
   context "#servers_domain_hosted_on?" do
-    it "should return a list of servers for a given domain name" do
-      dnp = DomainNameGem::Provider.new
-      dnp.adapter=(FakeAdapter)
+    it "return a list of servers" do
+      configuration = { :adapter => FakeAdapter }
+      dnp = DomainNameGem::Provider.new(configuration)
+
       expect(dnp.servers_domain_hosted_on?('setup_on_one_server.co.za')).to eql ['www10.tst1.host-h.net']
     end
 
-    it "Given the domain is not setup on any servers return a empty list" do
-      dnp = DomainNameGem::Provider.new
-      dnp.adapter=(FakeAdapter)
+    it "return an empty list if no results are found" do
+      configuration = { :adapter => FakeAdapter }
+      dnp = DomainNameGem::Provider.new(configuration)
+
       expect(dnp.servers_domain_hosted_on?('not_setup_on_any_servers.co.za')).to be_empty
     end
   end
 end
 
-module FakeAdapter
-  def self.servers_domain_hosted_on?(domain_name)
+class FakeAdapter
+  def initialize(configuration)
+    @configuration = configuration
+  end
+
+  def servers_domain_hosted_on?(domain_name)
     return ['www10.tst1.host-h.net'] if domain_name == 'setup_on_one_server.co.za'
     return [] if domain_name == 'not_setup_on_any_servers.co.za'
   end
